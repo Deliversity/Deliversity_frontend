dc9130bfc14d5beaa94197b8e5d3f83f5b2b5000
Seyeon Kim <rlatpdus321@ajou.ac.kr>
2020-11-14 22:44:18 +0900

    리뷰 작성하기 수정

--- src/screens/ManageDeliveryScreen.js
+++ src/screens/ManageDeliveryScreen.js
@@ -44,7 +44,7 @@ class ManageDeliveryScreen extends Component {
       <Card
         itemData={item}
         onPress={() => {
-          item.orderStatus === '3'
+          item.orderStatus === '3' && item.reviewedByRider === 0
             ? this.props.navigation.navigate('WriteReview', {
                 orderID: item.id,
                 riderID: item.riderId,
--- src/screens/ManageOrderScreen.js
+++ src/screens/ManageOrderScreen.js
@@ -29,7 +29,7 @@ class ManageOrderScreen extends Component {
       <Card
         itemData={item}
         onPress={() => {
-          item.orderStatus === '3'
+          item.orderStatus === '3' && item.reviewedByUser === 0
             ? this.props.navigation.navigate('WriteReview', {
                 orderID: item.id,
                 riderID: item.riderId,
