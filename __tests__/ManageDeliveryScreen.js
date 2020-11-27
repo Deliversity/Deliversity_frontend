9fbc4ba10afaae598935310c47d11734bbe13a59
seyeonHello <rlatpdus321@naver.com>
2020-11-28 00:55:17 +0900

    주문관리 배달관리 테스트

---
+++ __tests__/ManageDeliveryScreen.js
@@ -0,0 +1,27 @@
+import 'react-native';
+
+import React, {ReactElement} from 'react';
+import ManageDeliveryScreen from '../src/screens/ManageDeliveryScreen';
+import renderer from 'react-test-renderer';
+
+let props;
+let component: ReactElement;
+
+function getTempComponent(props) {
+  return <ManageDeliveryScreen {...props} />;
+}
+
+beforeAll(() => {
+  jest.mock('@react-native-community/async-storage');
+});
+
+describe('[Temp] render', () => {
+  props = {}; // fill test props
+  component = getTempComponent(props);
+  it('renders without crashing', () => {
+    const renderResult = renderer.create(component);
+    const renderedJson = renderResult.toJSON();
+    expect(renderedJson).toMatchSnapshot();
+    expect(renderedJson).toBeTruthy();
+  });
+});
