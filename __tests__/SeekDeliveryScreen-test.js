324d07c08b141edadd8d090eb9d009a0fe33a05a
seyeonHello <rlatpdus321@naver.com>
2020-12-06 19:34:09 +0900

    배달건찾기 ui 테스트

---
+++ __tests__/SeekDeliveryScreen-test.js
@@ -0,0 +1,30 @@
+import 'react-native';
+
+import React, {ReactElement} from 'react';
+import SeekDeliveryScreen from '../src/screens/SeekDeliveryScreen';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+configure({adapter: new Adapter()});
+let props;
+let component: ReactElement;
+
+beforeAll(() => {
+  jest.mock('@react-native-community/async-storage');
+});
+
+describe('[Temp] render', () => {
+  props = {}; // fill test props
+  const params = {};
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<SeekDeliveryScreen {...props} />);
+    wrapper.setState({params: params});
+  });
+  it('renders without crashing', () => {
+    // const renderResult = renderer.create(wrapper);
+    // const renderedJson = wrapper.toJSON();
+    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(toJson(wrapper)).toBeTruthy();
+  });
+});
