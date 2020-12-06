34d24c09315447f38b79fdc3036e8aea5588d252
seyeonHello <rlatpdus321@naver.com>
2020-12-06 14:58:58 +0900

    signup ui 테스트 수정

--- __tests__/SignupScreen-test.js
+++ __tests__/SignupScreen-test.js
@@ -3,7 +3,9 @@ import 'react-native';
 import React, {ReactElement} from 'react';
 import SignupScreen from '../src/screens/SignupScreen';
 import renderer from 'react-test-renderer';
-
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
 let props;
 let component: ReactElement;
 
@@ -16,12 +18,25 @@ beforeAll(() => {
 });
 
 describe('[Temp] render', () => {
-  props = {}; // fill test props
-  component = getTempComponent(props);
+  props = {
+    route: {
+      params: {
+        idToken: 'randomToken',
+      },
+    },
+  }; // fill test props
+  const params = {
+    idToken: 'randomToken',
+  };
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<SignupScreen {...props} />);
+    wrapper.setState({params: params});
+  });
   it('renders without crashing', () => {
-    const renderResult = renderer.create(component);
-    const renderedJson = renderResult.toJSON();
-    expect(renderedJson).toMatchSnapshot();
-    expect(renderedJson).toBeTruthy();
+    // const renderResult = renderer.create(component);
+    // const renderedJson = renderResult.toJSON();
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toBeTruthy();
   });
 });
